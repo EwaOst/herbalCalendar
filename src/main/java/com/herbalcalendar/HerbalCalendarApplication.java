4fa8965e8b57bb40eea75c370ae69ddb58fcc2b3
EwaOstromecka <ewaostromecka@gotmail.com>
2025-02-14 11:43:27 +0100

    first commit

---
+++ src/main/java/com/herbalcalendar/HerbalCalendarApplication.java
@@ -0,0 +1,11 @@
+package com.herbalcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerbalCalendarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HerbalCalendarApplication.class, args);
+    }
+}
